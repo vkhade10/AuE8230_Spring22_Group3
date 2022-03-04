7b56251bbd1e3bd84d7cada89e3c91e030e25fc4
Vishal1711 <jadhavvishal669@gmail.com>
2022-03-04 17:49:07 -0500

    Video added

--- assignment5_wallfollowingandobstacleavoidance/src/scripts/wall_follower.py
+++ assignment5_wallfollowingandobstacleavoidance/src/scripts/wall_follower.py
@@ -3,29 +3,24 @@
 import rospy
 from sensor_msgs.msg import LaserScan
 from geometry_msgs.msg import Twist
-import numpy as np 
-import math
-from nav_msgs.msg import Odometry
-from std_msgs.msg import String
+ 
+
 
 def callback(msg):
-    sweep = list(msg.ranges[0:360])
-    sweep = [v for v in sweep if not v > 5]
-    left_sweep = sweep[10:90]
-    right_sweep = sweep[270:350]
-    mean_right = sum(right_sweep)/len(right_sweep)
+    sweep = list(msg.ranges[0:360])                         # Make the list of laser scan data
+    sweep = [v for v in sweep if not v > 5]                 # Filter out inf data
+    left_sweep = sweep[10:90]                               # Took value from 10 degree to 90 degree for left sweep
+    right_sweep = sweep[270:350]                            # Took value from 270 degree to 350 for left sweep 
+    mean_right = sum(right_sweep)/len(right_sweep)       
     mean_left = sum(left_sweep)/len(left_sweep)
-    error = mean_left - mean_right
+    error = mean_left - mean_right                           # Error is defied as average of left distances minus average of right distance
 
+    velocity = 0.2                                           # Kept constant velocity
 
-    velocity = 0.2
+    kp = 0.2                                                 # Proportional coefficient
 
-    kp = 0.15
-
-    
-
-    if (error > 0.05 ):
-        velocity_msg.angular.z = -kp * error 
+    if (error > 0.05 ):                                      # Condition for error condition
+        velocity_msg.angular.z = kp * error 
         velocity_msg.linear.x = velocity
 
     elif (error < -0.05):
