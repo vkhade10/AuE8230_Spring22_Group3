b5dff260f54a1b522ec950073b82fa57e748dff9
Harshal Varpe <hvarpe@clemson.edu>
2022-03-09 14:51:27 -0500

    changed the PD controller code for wall_follower

--- assignment5_wallfollowingandobstacleavoidance/src/scripts/wall_follower.py
+++ assignment5_wallfollowingandobstacleavoidance/src/scripts/wall_follower.py
@@ -4,11 +4,17 @@ import rospy
 from sensor_msgs.msg import LaserScan
 from geometry_msgs.msg import Twist
 
+
+e11 = 0
+e21 = 0
+e31 = 0
+u = 0
+
 def callback(msg):
     sweep = list(msg.ranges[0:359]) # lidar reading 
     sweep = [i for i in sweep if not i>4] # ignoring values above 4 m
-    front1 = sweep[0:10]
-    front2 = sweep[-11:]
+    front1 = sweep[0:15]
+    front2 = sweep[-15:]
     front_dist = front1 + front2
     front_dist = sum(front_dist) / len(front_dist) # taking average of 20 degree lidar sweep
 
@@ -24,8 +30,12 @@ def callback(msg):
 
 # following lines and section is used for declaring discretised PD controller
     vel = 0.5 # velocity value
-    e11,e21,e31 = 0,0,0
-    u = 0
+    
+    global e11
+    global e21
+    global e31
+    global u
+    rospy.loginfo(" e11 %.2f ", e11)
     kp = 0.4
     ki = 0.0
     kd = 0.2
@@ -47,7 +57,7 @@ def callback(msg):
         # print(front_dist)
         # print("moving backwards")
         vel_msg.linear.x = vel 
-        vel_msg.angular.z = 1.5* error
+        vel_msg.angular.z = 1.2 * error
         
 
 rospy.init_node('wall_follow', anonymous=True)
